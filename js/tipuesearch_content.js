12078be9d5e2c4903a20fc919e4e5d226fd163ff
Novice-Koder <87114401+Novice-Koder@users.noreply.github.com>
2022-05-11 15:15:55 +0530

    Add files via upload

---
+++ js/tipuesearch_content.js
@@ -0,0 +1,10 @@
+var tipuesearch = {"pages": [
+    {"title": "HTML Objective ", "text": "We are trying to make all those topics more simple and easy to understand,So that it will help students to learn it very quickly in limited amount of time like Last Peak hours of their Extermination,its like last time revision Notes. we also have provided the depth knowledge of some topics which really require more words to explain.", "tags": "HTML", "loc": "http://127.0.0.1:5500/HTML/"},
+    {"title": "Computer Fundamental Objective", "text": "This is a demo of Tipue Search Static mode.", "tags": "", "loc": "http://www.tipue.com/search/demos/static"},
+    {"title": "Software Engineering Objective", "text": "This is a demo of Tipue Image Search.", "tags": "", "loc": "http://www.tipue.com/search/demos/images"},
+    {"title": "C++ Programming Ocjective", "text": "If you haven't already done so, download Tipue Search. Copy the tipuesearch folder to your site.", "tags": "documentation", "loc": "http://www.tipue.com/search/docs"},     
+    {"title": "Operating System Objective", "text": "Tipue drop is a search suggestion box jQuery plugin. Tipue drop is open source and released under the MIT License, which means it's free for both commercial and non-commercial use. Tipue drop is responsive and works on all reasonably modern browsers.", "tags": "JavaScript", "loc": "http://www.tipue.com/drop"},
+    {"title": "Tipue drop demo", "text": "Tipue drop demo. Tipue drop is a search suggestion box jQuery plugin.", "tags": "JavaScript", "loc": "http://www.tipue.com/drop/demo"},
+    {"title": "Support plans", "text": "Stuck? We offer a range of flexible support plans for our jQuery plugins.", "tags": "", "loc": "http://www.tipue.com/support"},
+    {"title": "About Tipue", "text": "Tipue is a small web development studio based in North London. We've been around for over a decade. We like Perl, MySQL and jQuery.", "tags": "", "loc": "http://www.tipue.com/about"}
+]};
